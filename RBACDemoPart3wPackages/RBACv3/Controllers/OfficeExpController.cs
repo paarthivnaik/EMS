6bffff4c37c554c41f72ebd64d16cded82e6b3a1
err <rtt@gmail.com>
2019-01-11 09:50:02 +0530

    Added New Screen for Expenses
    Added New Screen for Expense

---
+++ RBACDemoPart3wPackages/RBACv3/Controllers/OfficeExpController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RBACv3.Controllers
+{
+    public class OfficeExpController : Controller
+    {
+        // GET: OfficeExp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: OfficeExp/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OfficeExp/Create
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: OfficeExp/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OfficeExp/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OfficeExp/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OfficeExp/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OfficeExp/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RBACDemoPart3wPackages/RBACv3/Controllers/OfficeExpDataController.cs
@@ -0,0 +1,44 @@
+﻿using Events.Entities.Models;
+using Events.Repo.OfficeExpenses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RBACv3.Controllers
+{
+    public class OfficeExpDataController : ApiController
+    {
+        private IOfficeExpensesRepo _expensesRepo = new OfficeExpensesRepo();
+        [HttpPost]
+        public async Task<long> Save(Expens obj)
+        {
+            obj.CreatedBy = User.Identity.GetUserId();
+            var result = await _expensesRepo.Save(obj);
+            return result;
+        }
+        [HttpPost]
+        public async Task<long> Update(Expens obj)
+        {
+            obj.ModifiedBy = User.Identity.GetUserId();
+            var result = await _expensesRepo.Update(obj);
+            return result;
+        }
+        [HttpGet]
+        public async Task<Expens> GetById(long eventInfoId)
+        {
+            var result = await _expensesRepo.GetById(eventInfoId);
+            return result;
+        }
+        [HttpGet]
+        public async Task<List<Expens>> GetAll()
+        {
+            var result = await _expensesRepo.GetAll();
+            return result;
+        }
+        
+    }
+}
